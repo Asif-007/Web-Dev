41c853cdb61d5d1144f1e863ecb6193c23485c64
Asif <asifalishaan7@gmail.com>
2025-12-24 12:57:32 +0530

    Lecture 1 and 2

---
+++ JavaScript/Lecture-2/script.js
@@ -0,0 +1,93 @@
+// let name = "Asif";
+
+// let age = 18;
+
+// age = 30;
+
+// console.log(name, age);
+
+// const account = 1234;
+// account = 12;
+// console.log(account);
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+// a = 10;
+
+// if (true) {
+//     let a = 30;
+// }
+
+// console.log(a);
+
+// data types
+
+// primitive data types -> number,string, boolean, undefined, null, bigint, symbol
+
+// let a = 10;
+// let b = 2.36;
+// console.log(a, b);
+
+// let c = "Asif";
+// console.log(typeof (c));
+// let d = "Akmal";
+
+// console.log(c, d);
+
+// let login = true;
+// let f = false;
+// console.log(login, f);
+
+// let user;
+// console.log(user);
+
+// let num = 11548413478745688497n;
+// console.log(num);
+
+// let weather = null;
+// console.log(weather);
+
+// const id1 = Symbol("id");
+
+// const id2 = Symbol("id");
+
+// console.log(id1 == id2);
+
+// Non primitive Data types -> array, object
+
+// let arr = [10, 20, 1, "Asif", true];
+
+// console.log(arr);
+
+// let user = {
+//     name: "Rohit",
+//     account: 12312,
+//     age: 18,
+//     category: 'gen'
+// };
+// console.log(obj);
+
+
+// let s = function add() {
+//   console.log("hello");
+// };
+// console.log(typeof s);
+
+// s();
+
+
+let arr = [10, 20, 30, 40];
+arr.push(50);
+console.log(arr);
+
+
+let obj = {
+    name: "mohan",
+    age: 20
+};
+
+let obj2 = obj;
+
+obj2.name = "Asif";
+console.log(obj2);
