b3c4eb385a11781198928413e5c5d17a3fbd9e81
Asif <asifalishaan7@gmail.com>
2025-12-28 12:24:34 +0530

    Lecture-4

---
+++ JavaScript/Lecture-4/script.js
@@ -0,0 +1,97 @@
+// Operators
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 * 3)
+// console.log(6 / 2)
+// console.log(2 ** 3)
+// console.log(5 % 2);
+
+
+// Asssignment Operators
+// let x = 20;
+// let y = 10;
+// x += y;
+// console.log(x);
+
+
+
+// Comparision
+// let x = "10";
+// let y = 10;
+// console.log(x >= y);
+
+
+// let z = "10";
+// let a = Number(z);
+// console.log(a);
+
+// console.log(x == y);
+// console.log(x === y);
+
+
+// let a = '121ac';
+// let b = Number(a);
+// console.log(b);
+
+
+// let a = 10;
+// let b = String(a);
+
+// console.log(b, typeof b);
+
+
+// let a = true
+// console.log(Number(undefined));
+
+// console.log(typeof String(false))
+
+// console.log(Boolean(0))
+
+
+// let a = 0.1;
+// let b = 0.2;
+// let c = a + b;
+// console.log(c);
+
+
+// console.log(undefined === null);
+
+// console.log(null == 0)
+
+
+// console.log("Rohit" > "mohit");
+
+// console.log(NaN == NaN);
+
+
+// for (let i = 0; i <= 10; ++i){
+//     console.log(i);
+// }
+// let i = 1;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// }while(i<10)
+
+// let age = 25;
+// if (age >= 25)
+//     console.log("Adult");
+// else
+//     console.log("Old");
+
+
+// let a = "Rohit";
+// let b = "Mohit";
+// console.log(a && b);
+
+
+// console.log(2 & 3);
+// console.log(2 | 3);
+// console.log(!(2));
