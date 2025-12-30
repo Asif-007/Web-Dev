6e5fba1efdae888254202ccc2b606d25ce15fbbf
Asif <asifalishaan7@gmail.com>
2025-12-30 16:41:42 +0530

    Lecture-6

---
+++ JavaScript/Lecture-6/script.js
@@ -0,0 +1,55 @@
+// const str1 = "Rohit";
+// const str2 = "Rohit Negi";
+// const day = 18;
+// const str3 = `Strike is coming soon ${day}`;
+// console.log(str1, str2, str3);
+
+const str = `Hello Coder Army`;
+// console.log(str4.length);
+// console.log(str4[4]);
+// const str = str4.toUpperCase();
+// const b = str.toLowerCase();
+// console.log(str, b);
+
+// console.log(str.substring(0, 3));
+// console.log(str.indexOf('Cod'));
+// console.log(str.includes("Cod"));
+
+// console.log(str.slice(2, 7));
+
+// console.log(str.slice(-16,-12));
+
+// const a = "Rohit ";
+// const b = "Negi";
+// const c = a + b;
+// console.log(c);
+// const a = str.replace("ode", "iam");
+// console.log(a);
+
+// const user = "              Rohit Negi         ";
+// console.log(user.trim());
+
+// const name = "Rohit,Mohit,suraj,Rohan";
+// console.log(name.split(","));
+
+
+
+
+
+
+// const now = new Date();
+// console.log(now);
+// console.log(now.toString());
+
+
+// console.log(now.toLocaleDateString());
+
+// console.log(now.getMonth());
+
+
+// const now = new Date(2025, 8, 20, 8, 25, 16, 125);
+// console.log(now.toString());
+
+// const now = Date.now();
+// const dates = new Date(0);
+// console.log(dates.toString());
