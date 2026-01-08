6f0bf748106d56b1d452c74ed4e0ec06ca832b5f
Asif <asifalishaan7@gmail.com>
2026-01-08 15:21:52 +0530

    8 Porjects Done

---
+++ JavaScript/Lecture-16/08/script.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body");
+
+body.addEventListener("click", (e) => {
+  console.log(e.clientX, e.clientY);
+  const circleElement = document.createElement("div");
+  circleElement.classList.add("circle");
+
+  const color = ["red", "blue", "orange", "green", "pink", "purple"];
+  circleElement.style.backgroundColor =
+    color[Math.floor(Math.random() * color.length)];
+
+  circleElement.style.top = `${e.clientY-25}px`;
+  circleElement.style.left = `${e.clientX-25}px`;
+
+  body.append(circleElement);
+  setTimeout(() => {
+      circleElement.remove();
+      console.log(circleElement.isConnected)
+  }, 1000);
+});
